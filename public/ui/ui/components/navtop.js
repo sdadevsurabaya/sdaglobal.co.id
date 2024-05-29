37c7e348920d8405bfea681fbfdbd0dc4c0a72ef
pradhokot <99374274+pradhokot@users.noreply.github.com>
2024-05-29 15:50:26 +0700

    develop page leadership & page way SDA

--- public/ui/ui/components/navtop.js
+++ public/ui/ui/components/navtop.js
@@ -1,16 +1,15 @@
 document.write(`
 <div class="navtop bg-light py-3 py-lg-4">
    <div class="container d-lg-grid row-gap-lg-4">
-      <ul class="list-unstyled m-0 d-grid align-items-center d-lg-block"
-         style="grid-template-columns: 1fr auto 1fr;">
+      <ul class="list-unstyled m-0 d-grid align-items-center d-lg-block" style="grid-template-columns: 1fr auto 1fr;">
          <li class="d-lg-none">
             <button data-bs-toggle="offcanvas" data-bs-target="#sidebar">
                <i class="fas fa-bars fa-xl"></i>
             </button>
          </li>
          <li>
-            <h1 class="navbrand m-0">
-               <a href="index.html" class="d-flex justify-content-center">
+            <h1 class="navbrand m-0 text-center">
+               <a href="index.html">
                   <img src="../assets/img/logo/logo-sda.svg" width="auto" alt=""
                      style="height: calc(32px + 1vw);">
                </a>
@@ -22,18 +21,16 @@ document.write(`
             </button>
          </li>
       </ul>
-      <div class="offcanvas-lg offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="sidebar"
-         aria-labelledby="sidebarLabel">
+      <div class="offcanvas-lg offcanvas-start" data-bs-backdrop="static" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
          <div class="offcanvas-header">
             <button type="button" class="ms-auto" data-bs-toggle="offcanvas" data-bs-target="#sidebar">
                <i class="fas fa-close fa-2x"></i>
             </button>
          </div>
          <div class="offcanvas-body">
-            <ul class="list-unstyled m-0 text-capitalize d-flex flex-column row-gap-3 flex-lg-row flex-wrap justify-content-center w-100 column-gap-4"
-               style="font-size: large;">
+            <ul class="list-unstyled m-0 text-capitalize d-flex flex-column row-gap-3 flex-lg-row flex-wrap justify-content-center w-100 column-gap-4" style="font-size: large;">
                <li>
-                  <a href="#">home</a>
+                  <a href="index.html">home</a>
                </li>
                <li class="dropdown">
                   <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown"
@@ -52,16 +49,8 @@ document.write(`
                      </li>
                   </ul>
                </li>
-               <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" role="button" data-bs-toggle="dropdown"
-                     aria-expanded="false">
-                     our businesses
-                  </a>
-                  <ul class="dropdown-menu rounded-0">
-                     <li class="dropdown-item"><a href="#">Action</a></li>
-                     <li class="dropdown-item"><a href="#">Another action</a></li>
-                     <li class="dropdown-item"><a href="#">Something else here</a></li>
-                  </ul>
+               <li>
+                  <a href="#">our businesses</a>
                </li>
                <li>
                   <a href="#">news</a>
