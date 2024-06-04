62042b0327e9c8843b70caca72f9c511118ef44f
pradhokot <99374274+pradhokot@users.noreply.github.com>
2024-06-04 09:40:35 +0700

    revisi footbar hidden connect section

--- public/ui/components/footbar.js
+++ public/ui/components/footbar.js
@@ -4,7 +4,7 @@ document.write(`
       <div class="mobile d-lg-none">
          <div class="container d-grid row-gap-5">
             <script src="ui/components/footbar-nav1.js"></script>
-            <script src="ui/components/footbar-nav2.js"></script>
+            <!-- <script src="ui/components/footbar-nav2.js"></script> -->
             <script src="ui/components/footbar-img-industrial.js"></script>
             <script src="ui/components/footbar-sosmed.js"></script>
             <script src="ui/components/footbar-signup.js"></script>
@@ -16,24 +16,22 @@ document.write(`
          <div class="container d-grid" style="grid-template-columns: 1fr auto auto; column-gap: 5vw;">
             <div class="d-flex flex-column justify-content-between row-gap-4">
                <script src="ui/components/footbar-nav1.js"></script>
-               <script src="ui/components/footbar-sosmed.js"></script>
-               </div>
-               <div class="d-flex flex-column justify-content-between row-gap-4">
-               <script src="ui/components/footbar-img-industrial.js"></script>
-               <!--
-               <script src="ui/components/footbar-mobile.js"></script>
-               -->
-               <script src="ui/components/footbar-signup.js"></script>
             </div>
             <div class="d-flex flex-column justify-content-between row-gap-4">
-               <script src="ui/components/footbar-nav2.js"></script>
+               <script src="ui/components/footbar-img-industrial.js"></script>
+               <script src="ui/components/footbar-sosmed.js"></script>
+               <!-- <script src="ui/components/footbar-mobile.js"></script> -->
+            </div>
+            <div class="d-flex flex-column justify-content-between row-gap-4">
+               <script src="ui/components/footbar-signup.js"></script>
                <script src="ui/components/footbar-feedback.js"></script>
+               <!-- <script src="ui/components/footbar-nav2.js"></script> -->
             </div>
          </div>
       </div>
    </div>
    <div class="footbar-bottom bg-black text-white py-4">
-      <div class="container d-grid row-gap-4 d-lg-flex row-gap-lg-0 column-gap-5 justify-content-between">
+      <div class="container d-flex flex-column row-gap-4 justify-content-center flex-lg-row row-gap-lg-0 column-gap-lg-5 justify-content-lg-between">
          <nav class="custom-breadcrumb text-capitalize">
             <a href="#" target="_blank">privacy policy</a>
             <a href="#" target="_blank">terms & conditions</a>
