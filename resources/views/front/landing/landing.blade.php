a9aa5f863262bd370043b0a197875d03cc5a3274
Danu-Nur <diramadhan13@gmail.com>
2024-06-21 14:52:47 +0700

    hide carousel on banner landing

--- resources/views/front/landing/landing.blade.php
+++ resources/views/front/landing/landing.blade.php
@@ -22,31 +22,30 @@
 <section>
     <div id="slider" class="carousel slide carousel-fade" data-bs-ride="carousel" data-bs-interval="3000"
         data-bs-pause="false">
-        <div class="carousel-indicators">
-            <button type="button" data-bs-target="#slider" data-bs-slide-to="0" class="active" aria-current="true"
-                aria-label="Slide 1"></button>
+        {{-- <div class="carousel-indicators">
+            <button type="button" data-bs-target="#slider" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#slider" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#slider" data-bs-slide-to="2" aria-label="Slide 3"></button>
-        </div>
+        </div> --}}
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <img src="{{asset('assets/img/banner/banner.png')}}" class="d-block w-100" alt="...">
             </div>
-            <div class="carousel-item">
+            {{-- <div class="carousel-item">
                 <img src="{{asset('assets/img/banner/banner.png')}}" class="d-block w-100" alt="...">
             </div>
             <div class="carousel-item">
                 <img src="{{asset('assets/img/banner/banner.png')}}" class="d-block w-100" alt="...">
-            </div>
+            </div> --}}
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
+        {{-- <button class="carousel-control-prev" type="button" data-bs-target="#slider" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#slider" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
-        </button>
+        </button> --}}
     </div>
 </section>
 <section class="d-lg-flex text-split-lg-50">
