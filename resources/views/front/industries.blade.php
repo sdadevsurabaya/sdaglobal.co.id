be0210fca30c22ce23acbe8d65fd8d6f60c9ccdb
pradhokot <99374274+pradhokot@users.noreply.github.com>
2024-07-04 09:44:22 +0700

    template industries done

--- resources/views/front/industries.blade.php
+++ resources/views/front/industries.blade.php
@@ -1,13 +1,263 @@
 @extends('front/layout/layout')
 @section('pageStyles')
-<style></style>
+<style>
+    @media (min-width: 992px) {
+        .card-img-overlay {
+            transform: translateY(100%);
+            transition: all .36s ease-in-out;
+        }
+
+        .card:hover .card-img-overlay {
+            transform: translateY(0);
+        }
+
+        .card img {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%) scale(1.2);
+            opacity: .65;
+            transition: all .36s ease-in-out;
+        }
+
+        .card:hover img {
+            transform: translate(-50%, -50%) scale(1);
+            opacity: 1;
+        }
+    }
+</style>
 @endsection
 @section('content')
 <section>
+    <div class="page-banner py-5" style="background-image: url({{asset('assets/img/industries/banner-industries.jpg')}})">
+        <div class="container py-5">
+            <div class="row row-cols-md-auto py-5">
+                <div class="col py-5">
+                    <div class="p-4 bg-primary text-light px-md-5">
+                        <h5 class="display-4 text-center text-md-start">
+                            INDUSTRIES
+                            <br><b class="fw-bold">WE SERVE</b>
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section>
     <div class="container">
-        <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. At, saepe. Odio consequatur similique a minima animi voluptatum dignissimos voluptatem aperiam dolorem reiciendis rerum aliquid tempore nostrum molestias, porro iure quaerat.
-        </p>
+        <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-3 g-lg-4 g-xxl-5">
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/agriculture.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">Agriculture</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/automotive.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">automotive</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/construction.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">construction</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/consumer-product.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">consumer products</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/f&b.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">food & beverage</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/manufacturing.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">general manufacturing</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/industiral.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">heavy duty industrial equipments</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/hvac.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">HVAC</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/garden.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">lawn, garden & professional lanscaping</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/machines.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">machine tools & precision equipment</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/micromobility.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">micromobility</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/mining.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">mining</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/oil.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">oil & gas</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/sports.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">powersports</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+            <div class="col">
+                <a href="#" class="text-decoration-none">
+                    <div class="card overflow-hidden">
+                        <div class="ratio ratio-16x9 bg-secondary">
+                            <img src="{{asset('assets/img/industries/truck.jpg')}}" class="object-fit-cover" alt="...">
+                        </div>
+                        <div class="card-img-overlay bg-primary text-light top-unset text-center">
+                            <h5 class="card-title text-capitalize">
+                                <b class="fw-bold">truck & bus</b>
+                            </h5>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </section>
 @endsection
