0db05aebf4ed5d2736d3219ec5b83b44d088ab34
suryo <suryoatm@gmail.com>
2024-07-11 16:19:50 +0700

    hide ,

--- resources/views/front/layout/footbar/footbar-hotline.blade.php
+++ resources/views/front/layout/footbar/footbar-hotline.blade.php
@@ -1,5 +1,5 @@
 <h5 class="fs-4">
     <b class="fw-bold">
-        <i class="fas fa-phone-volume fa-xl mx-2 d-none d-lg-inline"></i> <small class="text-nowrap"><small><small>Hotline : </small></small></small><a href="tel:+623199008800" target="_blank" class="text-nowrap">+62 31 9900 8800</a>, <small class="text-nowrap"><small><small class="ms-lg-5">WA Only : </small></small></small><a href="https://wa.me/+6282280808800" target="_blank" class="text-nowrap">+62 822 8080 8800</a>
+        <i class="fas fa-phone-volume fa-xl mx-2 d-none d-lg-inline"></i> <small class="text-nowrap"><small><small>Hotline : </small></small></small><a href="tel:+623199008800" target="_blank" class="text-nowrap">+62 31 9900 8800</a> <small class="text-nowrap"><small><small class="ms-lg-5">WA Only : </small></small></small><a href="https://wa.me/+6282280808800" target="_blank" class="text-nowrap">+62 822 8080 8800</a>
     </b>
 </h5>
